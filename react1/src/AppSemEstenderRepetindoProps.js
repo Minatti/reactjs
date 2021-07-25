727b429108c0b43e26b385249fc6175dbda4ad46
Luan Carminatti <luan@savarveiculos.com.br>
2021-07-25 12:53:55 -0300

    maneira nao muito pratica e trabalhosa

---
+++ react1/src/AppSemEstenderRepetindoProps.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+// Utilizando biblioteca do styled
+// Simples e fácil de aplicar
+// Usado para estrutura base do app
+
+/* Não seria muito prático repetir o estilo existente, então estendemos o componente para reutilizar */
+ 
+const Site = styled.div`
+    width:400px;
+    height:400px;
+    background-color: #d9d9d9;
+`;
+
+const Botao = styled.button`
+    color: #FF0000;
+    padding: 10px 15px;
+    font-size: 19px;
+    border: 3px solid #FF0000;
+    color: $FF0000;
+    background-color: #FFF;
+    margin: 5px;
+    border-radius: 5px;
+`;
+
+
+const BotaoPequeno = styled.button`
+    color: #FF0000;
+    padding: 5px 10px;
+    font-size: 19px;
+    border: 2px solid #FF0000;
+    color: $FF0000;
+    background-color: #FFF;
+    margin: 5px;
+    border-radius: 5px;
+`;
+
+
+
+function App(){
+    return (
+        <Site>
+           <Botao>Clique Aqui</Botao>
+           <BotaoPequeno>Clique Aqui</BotaoPequeno>
+        </Site>
+    );
+}
+
+
+export default App;
